087bc7ace5e3bab694c847f0037728d83c46da4a
rahman Orewole <rorewole@gmail.com>
2025-03-19 10:33:22 +0100

    adding proxy

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,23 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { createDirectoryIfNotExists, readJSON, writeDataToFile } from "./utils/helper";
 import { generateDocs } from "./utils/makeRequest";
+import http from "http"
+import express from "express"
+import bodyParser from "body-parser";
+// import axios from "axios";
+const mairaPort = 8081;
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+  console.log('Request Method:', req.method);
+  console.log('Request Path:', req.path);
+  console.log('Request Body:', req.body);
+  console.log('Request Query:', req.query);
+  console.log('Request Params:', req.params);
+  next();
+});
 
 (async function () {
   const argv = await yargs(hideBin(process.argv)).scriptName("maira-cli").usage("$0 <cmd> [args]").command(
@@ -39,6 +56,13 @@ import { generateDocs } from "./utils/makeRequest";
           alias: "n",
           demandOption: true,
         });
+    }).command("http", "proxy requests", (yargs) => {
+      yargs.option("port", {
+        describe: "The port to forward request to",
+        type: "number",
+        alias: "p",
+        demandOption: true
+      })
     }).help().argv;
 
     if (argv._[0] == "generate" ){
@@ -53,4 +77,26 @@ import { generateDocs } from "./utils/makeRequest";
         await writeDataToFile(writePath, JSON.stringify(data, null, "\t"))
         console.log(`Documentation generated and saved to ${writePath}. Preview: https://maira-virid.vercel.app/?id=${jsonId}`)
     }
+    if (argv._[0] == "http"){
+      const {port} = argv
+      // const targetUrl = 'http://localhost:'+port;
+      app.all("*", async function(req, res){
+        const {method, path, headers, body, query, params, url} = req
+        console.log({
+          method,
+          url: url+path,
+          headers,
+          data: body,
+          query,
+          params,
+        })
+        res.status(200).send("ok")
+      })
+      const server = http.createServer(app)
+      server.listen(mairaPort, ()=> {
+        console.log("forwarding requests from port ", mairaPort)
+      })
+      console.log("forwarding requests to http://localhost:"+port)
+    }
+    
 })();
